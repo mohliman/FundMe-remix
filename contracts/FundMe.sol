68055dcda4ac940dbf9c3b0cf2c22110c6b891dd
mohliman <bankitliman@gmail.com>
2025-08-02 23:18:41 +0100

    initial commit

---
+++ contracts/FundMe.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+// get funds from users
+// withdraw funds
+// set a minimum funding value in USD
+contract FundMe {
+    function fund() public {
+        
+    }
+
+
+    //function withdraw() public {}
+    
+}
